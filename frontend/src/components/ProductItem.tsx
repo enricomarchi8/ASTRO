85a76c3f4e7d65b96d68229e4729798d5122922b
enricomarchi8 <enrico.marchi8@studio.unibo.it>
2023-12-28 17:09:08 +0100

    Fixed bug

--- frontend/src/components/ProductItem.tsx
+++ frontend/src/components/ProductItem.tsx
@@ -1,26 +1,37 @@
-import { Product } from "../types/Product"
-import { Card } from 'react-bootstrap'
+import { Button, Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { Product } from "../types/Product";
+import Rating from "./Rating";
 
-function ProductItem({ product }: { product: Product}) {
-    return <Card>
-            <Link to={`/product/${product.slug}`}>
-                <img src={product.image} className="card-img-top" alt={product.nome} />
-            </Link>
-            <Card.body>
-              <Link to={`/product/${product.slug}`}>
-                <Card.Tittle>{product.nome}</Card.Tittle>
-              </Link>
-              <Rating rating={product.valutazione} numReviews={product.numRecensioni} />
-              <Card.Text>${product.price}</Card.Text>
-              y{product.disponibilita === 0 ?(
-                <Button variant="light" disabled>
-                    Non disponibile
-                </Button>
-              ) : (
-                <Button>Aggiungi al Carrello</Button>
-              )}
-            </Card.body>
+function ProductItem({ product }: { product: Product }) {
+  return (
+    <Card>
+      <Link to={`/product/${product.slug}`}>
+        <img
+          src={product.immagine}
+          className="card-img-top"
+          alt={product.nome}
+        />
+      </Link>
+      <Card.Body>
+        <Link to={`/product/${product.slug}`}>
+          <Card.Title>{product.nome}</Card.Title>
+        </Link>
+        <Rating
+          rating={product.valutazione}
+          numReviews={product.numRecensioni}
+        />
+        <Card.Text>${product.prezzo}</Card.Text>
+        {product.disponibilita === 0 ? (
+          <Button variant="light" disabled>
+            Non disponibile
+          </Button>
+        ) : (
+          <Button>Aggiungi al Carrello</Button>
+        )}
+      </Card.Body>
     </Card>
+  );
 }
 
-export default ProductItem
+export default ProductItem;
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,29 +1,5 @@
-import { Col, Row } from 'react-bootstrap'
-import LoadingBox from '../components/LoadingBox'
-import MessageBox from '../components/MessageBox'
-import ProductItem from '../components/ProductItem'
-import { Helmet } from 'react-helmet-async'
-import { useGetProductsQuery } from '../hooks/productHooks'
-import { getError } from '../utils'
-import { ApiError } from '../types/ApiError'
+import React from "react";
 
-
-export default function HomePage() {
-  const { data: products, isLoading, error } = useGetProductsQuery()
-  return isLoading ? (
-    <LoadingBox />
-  ) : error ? (
-    <MessageBox variant="danger">{getError(error as ApiError)}</MessageBox>
-  ) : (
-    <Row>
-      <Helmet>
-        <title>ASTRO</title>
-      </Helmet>
-      {products!.map((product) => (
-        <Col key={product.slug} sm={6} md={4} lg={3}>
-          <ProductItem product={product} />
-        </Col>
-      ))}
-    </Row>
-  )
+export default function Home() {
+  return <div>Home</div>;
 }
